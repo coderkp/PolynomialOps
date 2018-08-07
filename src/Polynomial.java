1a2b2f50c18a843b8225cbc5f8fd6be1873398e0
Kush Parikh <kushparikh01@gmail.com>
2018-08-07 09:46:35 +0530

    Initial commit - 4

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -6,6 +6,23 @@ public class Polynomial {
     }
     public int[] pre_process(String s)
     {
+        String temp= "";
+        if(s.charAt(0)!= '-')
+            temp = temp + "+";
+        int highestPower=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)== '^'){
+                if(((s.charAt(i+1) - '0') > highestPower))
+                {
+                    highestPower = (s.charAt(i+1) - '0');
+                }
+            }
+            if(s.charAt(i) != ' ')
+                temp = temp +  s.charAt(i);
+        }
+        arr = new int[highestPower];
+        int j=0;
         return new int[1];
     }
 
