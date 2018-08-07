414d9f86bef1b3ce73ebe803efb52cbefc0475e7
Kush Parikh <kushparikh01@gmail.com>
2018-08-07 10:34:52 +0530

    pre-process

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+        Polynomial p = new Polynomial("3x^2 + 2x^7 - 3");
+
     }
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -2,10 +2,12 @@ public class Polynomial {
     private int arr[];
     Polynomial(String s)
     {
-
+        this.arr = pre_process(s);
+        System.out.println(arr);
     }
     public int[] pre_process(String s)
     {
+        int[] processed_arr;
         String temp= "";
         if(s.charAt(0)!= '-')
             temp = temp + "+";
@@ -21,9 +23,48 @@ public class Polynomial {
             if(s.charAt(i) != ' ')
                 temp = temp +  s.charAt(i);
         }
-        arr = new int[highestPower];
+        System.out.println(highestPower);
+        processed_arr = new int[highestPower + 1];
         int j=0;
-        return new int[1];
+        while(j<temp.length())
+        {
+            int mf= 0, coeff=0, power=0;
+            if(temp.charAt(j)== '+')
+                mf = 1;
+            else if(temp.charAt(j)== '-')
+                mf = -1;
+
+            j++;
+            if(temp.charAt(j)== 'x')
+                coeff = 1;
+            if((j+1)==temp.length() || (temp.charAt(j+1) == '+' || temp.charAt(j+1) == '-')) {
+                coeff = temp.charAt(j) - '0';
+                coeff*=mf;
+                power = 0;
+                j++;
+                processed_arr[power] = coeff;
+                continue;
+            }
+            if((j+2)==temp.length() || (temp.charAt(j+2) == '+' || temp.charAt(j+2) == '-'))
+            {
+                coeff = temp.charAt(j) - '0';
+                coeff*=mf;
+                power = 1;
+                j+=2;
+                processed_arr[power] = coeff;
+                continue;
+            }
+            else {
+                coeff = temp.charAt(j) - '0';
+                j++;
+            }
+            j+=2;
+            power = temp.charAt(j) - '0';
+            System.out.println(power);
+            processed_arr[power] = coeff*mf;
+            j++;
+        }
+        return processed_arr;
     }
     public String post_process(int[] arr){
         return "";
